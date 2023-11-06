8dacba05481573ac79e87cc13893fab9842975da
Praveen kumar naidu Korrapati <91273671+Praveenkumar0402@users.noreply.github.com>
2023-11-06 16:53:14 +0530

    Delete demo directory

--- demo/src/main/java/com/example/demo/exception/IdNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.exception;
-
-public class IdNotFoundException extends Exception {
-	
-	public IdNotFoundException() {
-		super("Id not found");
-	}
-
-}
--- demo/src/main/java/com/example/demo/exception/InvalidNameFormatException.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.exception;
-
-public class InvalidNameFormatException extends Exception {
-
-	public InvalidNameFormatException() {
-		super("Invalid name format ");
-	}
-}
